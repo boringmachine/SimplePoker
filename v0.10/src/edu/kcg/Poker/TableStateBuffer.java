17e34d4977b4f4438d3214e40e3835f0df093778
john <john@john-pc>
2013-02-03 06:52:20 +0900

    update

---
+++ v0.10/src/edu/kcg/Poker/TableStateBuffer.java
@@ -0,0 +1,13 @@
+package edu.kcg.Poker;
+
+import java.util.ArrayList;
+
+/**
+ * Tableのフィールドの状態をParamsとしてパックし追加する.
+ * 
+ * @author Shun.S
+ * 
+ */
+public class TableStateBuffer extends ArrayList<Params> {
+	private static final long serialVersionUID = 1L;
+}
