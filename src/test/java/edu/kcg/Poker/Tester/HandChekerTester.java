45fa3e3291db5f9010c313723c868391f1888fb9
boringmachine <machine449@gmail.com>
2013-04-20 19:45:47 +0900

    update

--- src/test/java/edu/kcg/Poker/Tester/HandChekerTester.java
+++ src/test/java/edu/kcg/Poker/Tester/HandChekerTester.java
@@ -1,157 +1,49 @@
 package edu.kcg.Poker.Tester;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import edu.kcg.Poker.Common.HandChecker;
 
+@RunWith(Theories.class)
+public class HandChekerTester {
 
-public class HandChekerTester{
+	@DataPoints
+	public static Fixture[] f = {
+			new Fixture(new int[]{0,1,2,3,4},HandChecker.SF | 4),
+			new Fixture(new int[]{39,48,49,50,51},HandChecker.SF | 13),
+			new Fixture(new int[]{0,0+13,0+26,0+39},HandChecker.FO),
+			new Fixture(new int[]{12,12+13,12+26,12+39},HandChecker.FO | 12),
+			new Fixture(new int[]{0,0+13,0+26,1,1+13},HandChecker.FU | 1),
+			new Fixture(new int[]{0,2,4,6,8},HandChecker.FL | 8),
+			new Fixture(new int[]{0,1,2,3,4+13},HandChecker.ST | 4),
+			new Fixture(new int[]{9,10,11,12,13},HandChecker.ST | 13),
+			new Fixture(new int[]{0,0+13,0+26},HandChecker.TH),
+			new Fixture(new int[]{0,0+13,3,3+13},HandChecker.TW | 3<<6),
+			new Fixture(new int[]{0,0+13},HandChecker.ON),
+			new Fixture(new int[]{12},12),
 
-	@Test
-	public void testStraightFlushByMax4() throws Exception{
-
-		// コミュニティカード
-		int[] com = { 0, 1, 4, 11, 12 };
-		// プレイヤーの手札
-		int handl = 2;
-		int handr = 3;
-		handl <<= 6;
-
-		int expected = HandChecker.SF | 4;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));
-	}	
+	};
 	
-	@Test
-	public void testFlushByMax12() throws Exception{
-
-		// コミュニティカード
-		int[] com = { 1, 5, 6, 50, 51 };
-		// プレイヤーの手札
-		int handl = 11;
-		int handr = 12;
-		handl <<= 6;
-
-		int expected = HandChecker.FL | 12;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));
+	@Theory
+	public void testCheckHand(Fixture f) throws Exception{
+		assertThat(HandChecker.checkHand(f.hand, f.com),is(f.expected));
 	}
 	
-	@Test
-	public void testStraightByMax4() throws Exception{
-		// コミュニティカード 1:C 3:S 5:S H:12 H:11  
-		int[] com = { 15, 39, 43 ,12, 11 };
+	static class Fixture{
+		int[] com;
+		int hand;
+		int expected;
 		
-		// プレイヤーの手札 4:H 2:D
-		int handl = 3;
-		int handr = 27;
-		handl <<= 6;
-
-		int expected = HandChecker.ST | 4;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));		
+		Fixture(int[] c, int e){
+			this.com = c;
+			this.hand = 0xF | 0xE <<6;
+			this.expected = e;
+		}
 	}
-	
-	@Test
-	public void testFullHouseBy1And2(){
-		// コミュニティカード  
-		int[] com = { 1, 14, 11 ,12, 2 };
-		
-		// プレイヤーの手札
-		int handl = 27;
-		int handr = 28;
-		handl <<= 6;
-
-		int expected = HandChecker.FU | 2;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));	
-	}
-	
-	@Test
-	public void testFourCardBy1(){
-		// コミュニティカード  
-		int[] com = { 1, 12, 11, 10, 14};
-		
-		// プレイヤーの手札
-		int handl = 27;
-		int handr = 40;
-		handl <<= 6;
-
-		int expected = HandChecker.FO | 1;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));	
-	}
-	
-	@Test
-	public void testThreeCardBy1(){
-		// コミュニティカード  
-		int[] com = { 1, 12, 11, 10, 19};
-		
-		// プレイヤーの手札
-		int handl = 27;
-		int handr = 40;
-		handl <<= 6;
-
-		int expected = HandChecker.TH | 1;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));	
-	}
-	
-	@Test
-	public void testTwoPairBy1And2(){
-		// コミュニティカード  
-		int[] com = { 1, 2, 11, 10, 19};
-		
-		// プレイヤーの手札
-		int handl = 27;
-		int handr = 41;
-		handl <<= 6;
-
-		int expected = HandChecker.TW | (2 << 6) | 1;
-		int actual = HandChecker.checkHand(handl | handr, com);
-		
-		assertThat(actual,is(expected));	
-	}
-	
-	@Test
-	public void testOnePairBy1(){
-		// コミュニティカード  
-		int[] com = { 1, 2, 11, 10, 19};
-				
-		// プレイヤーの手札
-		int handl = 27;
-		int handr = 43;
-		handl <<= 6;
-
-		int expected = HandChecker.ON | 1;
-		int actual = HandChecker.checkHand(handl | handr, com);
-				
-		assertThat(actual,is(expected));	
-	}
-	
-	@Test
-	public void testNoPairByMax7(){
-		// コミュニティカード  
-		int[] com = { 0, 1, 2, 3, 18};
-				
-		// プレイヤーの手札
-		int handl = 19;
-		int handr = 20;
-		handl <<= 6;
-
-		int expected = 7;
-		int actual = HandChecker.checkHand(handl | handr, com);
-				
-		assertThat(actual,is(expected));
-	}
-	
 }
